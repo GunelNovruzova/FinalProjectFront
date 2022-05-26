de636409c479dec02b833e26ee4481a4c010fe6c
GunelNovruzova <gunelvn@code.edu.az>
2022-05-26 14:50:24 +0400

    sidebar

---
+++ assets/js/side-bar.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".side-menu").click(function(){
+        $(this).toggleClass("side-menu-active");
+    })
+    $('.side-menu').click(function(){
+        // var navbar = document.querySelector(".nav-bar");
+        $(".nav-bar").slideToggle();
+        // navbar.classList.toggle('active-nav')
+       
+    })
+    $('.side-menu').click(function(){
+        // var navbar=document.querySelector("")
+        $(".nav-bar").slideToggle();
+        // navbar.classList.toggle('active-nav')
+    
+    })
+})
