c867b6bd0b28fa8011f28ac04af5daa7977d939d
GunelNovruzova <gunelvn@code.edu.az>
2022-05-28 16:01:49 +0400

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,51 +30,10 @@ $(document).ready(function () {
 	});
 });
 AOS.init();
-// var search = document.querySelector(".searchglass");
-// var closeIcon = document.querySelector(".mark");
-// search.addEventListener("click",function(e){
-//     e.preventDefault();
-//     var myinput = document.querySelector(".searchinput");
-//     myinput.classList.toggle("forsearch");
-// })
-// closeIcon.addEventListener("click",function(){
-//     var myinput = document.querySelector(".searchinput");
-//     myinput.classList.add("forsearch");
-// })
 
-// var searchBtn = document.querySelector(".searchglass");
-// searchBtn.addEventListener("click",function(e){
-//     e.preventDefault();
-//     var secFor = document.querySelector(".forsearch");
-//     secFor.style.height = "100%";
-//     secFor.style.opacity = "1";
-//     secFor.style.top = "0";
-//     secFor.style.visibility = "visible";
-// })
 
-// var closeIcon = document.querySelector(".mark");
-// closeIcon.addEventListener("click",function(){
-//     var secFor = document.querySelector(".forsearch");
-//     secFor.style.height = "0%";
-//     secFor.style.top = "0%";
-//     secFor.style.opacity = "0";
-//     secFor.style.visibility = "hidden";
-// })
+
+
+
   
-// var basket =document.querySelector(".basket");
-// basket.addEventListener("click",function(e){
-//     e.preventDefault();
-//     var basketfor=document.querySelector(".forbasket");
-//    basketfor.style.opacity="1";
-//     basketfor.style.visibility="visible";
-//     basketfor.style.right="1%"
-// })
-// var closebasket=document.querySelector(".closebasket");
-// closebasket.addEventListener("click",function(e){
-//     e.preventDefault();
-//     var basketfor= document.querySelector(".forbasket");
-//     basketfor.style.visibility="hidden";
-//     basketfor.style.right="-100%";
-//     basketfor.style.opacity="0";
-// })
 
--- assets/js/side-bar.js
+++ assets/js/side-bar.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
+
+    $(".nav-bar").slideUp();
+
     $(".side-menu").click(function(){
         $(this).toggleClass("side-menu-active");
-    })
-    $('.side-menu').click(function(){
-        // var navbar = document.querySelector(".nav-bar");
         $(".nav-bar").slideToggle();
-        // navbar.classList.toggle('active-nav')
-       
-    })
-    $('.side-menu').click(function(){
-        // var navbar=document.querySelector("")
-        $(".nav-bar").slideToggle();
-        // navbar.classList.toggle('active-nav')
-    
     })
 })
